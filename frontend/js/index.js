6fcdd9899a1610bec789ce9e07cbbc7599621bca
tehuel-de-barros <tehueldebarros@gmail.com>
2022-10-24 12:31:13 -0300

    segundo commit

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,38 +1,74 @@
 const url = '../backend/productos.json'
 const productos = [];
+const carrito = [];
 // Creamos a la funcion BD para que bsuque a la variable url, recuperamos los datos localizados en la var url, y los pusheamos a el array productos   
 
-
 const cat = document.getElementById('catalogo');
+const contCar = document.getElementById('contCar');
+const empty = document.getElementById('empty');
+const contadorCar = document.getElementById('contadorCar');
+let precio = document.getElementById('price');
 
 let catalogo = () => { 
-    productos.forEach(info => {
+    productos.forEach((info, index) => {
         //carta 
-        const card = document.createElement('div');
-        card.className = "card m-2 p-2";
-        card.style = 'width: 18rem';
-        // imagen 
-        const image = document.createElement('img')
-        image.src = info.foto;
-        image.className = "card-img-top";
-        image.alt = info.nombre;
-         // cuerpo de la carta
-         const cardBody = document.createElement('div');
-         cardBody.innerHTML = `
-         <h5>${info.nombre}</h5>
-         <p>$${info.precio} </p>
-     `; 
-        // boton
-        const button = document.createElement('button');
-        button.className = "btn btn-success";
-        button.innerText = "Agregar";
-        // 
-        cat.appendChild(card);
-        card.appendChild(image);
-        card.appendChild(cardBody);
-        cardBody.appendChild(button);
+        const div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML = `
+        <img src=${info.foto} alt= "${info.nombre}" class = "">
+        <h3>${info.nombre}</h3>
+        <p class="precioProducto">Precio:$ ${info.precio}</p>
+        <button id="agregar${info.id}" class="btn btn-success">Agregar <i class="fas fa-shopping-cart"></i></button>
+        `
+        cat.appendChild(div);
+        const button  = document.getElementById(`agregar${info.id}`);
+        button.addEventListener('click', () => {
+            agregarAlCarrito(info.id);
+        })
     })
 } 
+
+
+let agregarAlCarrito = (ID) => {
+    const items = productos.find(prod => prod.id === ID);
+    carrito.push(items);
+    renderCar();
+    console.log(carrito);
+}
+
+let renderCar = () => {
+    contCar.innerHTML = "";
+
+    carrito.forEach(producto => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${producto.nombre}</p>
+        <p>Precio:$${producto.precio}</p>
+        <p>Cantidad: <span id="cantidad">${producto.cantidad}</span></p>
+        <button onclick="eliminarCar(${producto.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+        contCar.appendChild(div);
+    })
+    contadorCar.innerText = carrito.length;
+    precio.innerText = carrito.reduce((contador, producto) => contador + producto.precio, 0);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+let eliminarCar = (ID) => { 
+    const items = carrito.find(prod => prod.id === ID);
+    let index = carrito.indexOf(items);
+    carrito.splice(index, 1);
+    renderCar();
+}
+
+let vaciarCar = () => {
+    carrito.length = 0;
+    renderCar();
+}
+empty.addEventListener('click', vaciarCar);
+
+
 
 let BD = () => { 
     fetch(url)
@@ -53,7 +89,11 @@ console.log(productos);
 
 
 
-
-
-
+// localStorage 
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'));
+        renderCar();
+    }
+})
 
